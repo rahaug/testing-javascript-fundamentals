fd469c1ae60473cf19a67e18a5d46f551048606b
Rolf Haug <rolf@rah.no>
2020-06-18 12:17:24 +0200

    5. Solutions

--- tests/user-service.test.js
+++ tests/user-service.test.js
@@ -2,3 +2,29 @@ import * as service from '../src/user-service'
 import axios from 'axios'
 
 const baseUrl = 'https://reqres.in/api/'
+
+jest.mock('axios')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+it('requests the correct endpoint when getting users', () => {
+  service.getUsers()
+  expect(axios.get).toHaveBeenCalledWith('https://reqres.in/api/users')
+})
+
+it('requests the correct endpoint when getting a specific users', () => {
+  service.getUser(1337)
+  expect(axios.get).toHaveBeenCalledWith('https://reqres.in/api/users/1337')
+  expect(axios.get).toHaveBeenCalledTimes(1)
+})
+
+it('requests the correct endpoint when creating a user', () => {
+  const payload = { first_name: 'Bruce', last_name: 'Wayne' }
+  service.storeUser(payload)
+  expect(axios.post).toHaveBeenCalledWith(
+    'https://reqres.in/api/users',
+    payload
+  )
+})
